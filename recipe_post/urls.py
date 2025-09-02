55ed8a88457122712f38a86bd45ab341668d3acb
Chandni-L5 <qssinhux@students.codeinstitute.net>
2025-09-02 22:46:20 +0100

    add recipe_page view and create recipe_page.html template

--- recipe_post/urls.py
+++ recipe_post/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecipeHubList.as_view(), name='home'),
+    path('<slug:slug>/', views.recipe_page, name='recipe_page')
 ]
--- recipe_post/views.py
+++ recipe_post/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import RecipePost
 
@@ -9,3 +9,25 @@ class RecipeHubList(generic.ListView):
     queryset = RecipePost.objects.order_by('-created_on').filter(status=1)
     template_name = 'recipe_post/index.html'
     paginate_by = 6
+
+
+def recipe_page(request, slug):
+    """
+    Displays an individual :model:'recipe_post.RecipePost'.
+
+    **Context**
+    ``post``
+        An instance of :model:'recipe_post.RecipePost'.
+
+    **Template**
+    :template:`recipe_post/recipe_page.html`
+    """
+    queryset = RecipePost.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    ingredients_list = [i.strip() for i in post.ingredients.split(',')]
+    return render(
+        request,
+        "recipe_post/recipe_page.html",
+        {"post": post, "ingredients_list": ingredients_list},
+    )
