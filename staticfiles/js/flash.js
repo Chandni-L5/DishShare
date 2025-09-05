7bcd7baedce7bb4539ba32a9fda024e6e559a47c
Chandni-L5 <qssinhux@students.codeinstitute.net>
2025-09-05 22:10:40 +0100

    set up staticfiles

---
+++ staticfiles/js/flash.js
@@ -0,0 +1,21 @@
+// script to hide the login status message after 20 seconds
+document.addEventListener('DOMContentLoaded', function() {
+    const msg = document.getElementById('login-status');
+    if (msg) {
+        setTimeout(() => {
+            msg.classList.add("flash-hide");
+            setTimeout(() => msg.remove(), 1000);
+        }, 5000);
+    }
+});
+
+// script to hide the logout status message after 20 seconds
+document.addEventListener('DOMContentLoaded', function() {
+    const msg = document.getElementById('logout-status');
+    if (msg) {
+        setTimeout(() => {
+            msg.classList.add("flash-hide");
+            setTimeout(() => msg.remove(), 1000);
+        }, 5000);
+    }
+});
