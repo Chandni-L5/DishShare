824c2b89728f136ea7bdc270802fce34bb101048
Chandni-L5 <qssinhux@students.codeinstitute.net>
2025-09-10 21:59:58 +0100

    update admin.py to display filters

--- recipe_post/admin.py
+++ recipe_post/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import RecipePost, Comment, Ingredients, Method
 from allauth.account.models import EmailAddress
+from django_summernote.models import Attachment
+from django.contrib.auth.models import Group
+from django.contrib.sites.models import Site
 
 
 class IngredientsInline(admin.TabularInline):
@@ -17,7 +20,10 @@ class MethodInline(admin.TabularInline):
     model = Method
     extra = 1
     fields = ("order", "text")
-    verbose_name_plural = "Method - Enter each step of the method individually."
+    verbose_name_plural = (
+        "Method - Enter each step of the method "
+        "individually."
+    )
 
 
 @admin.register(RecipePost)
@@ -25,8 +31,17 @@ class RecipePostAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "status", "created_on")
     prepopulated_fields = {"slug": ("title",)}
     inlines = [IngredientsInline, MethodInline]
+    list_filter = ("status", "created_on", "author")
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("recipe", "created_on", "approved")
+    list_filter = ("status", "created_on", "author")
 
 
 # Register your models here.
-admin.site.register(Comment)
 admin.site.unregister(EmailAddress)
+admin.site.unregister(Attachment)
+admin.site.unregister(Group)
+admin.site.unregister(Site)
